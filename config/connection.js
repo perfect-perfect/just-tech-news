8c264f45bc1c973b1921279a691bf600aaa4b287
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-03-14 21:28:29 -0400

    small change

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,7 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
-}
-else {
+} else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
